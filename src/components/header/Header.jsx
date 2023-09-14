656c422b6df0562a247a7b520483602d8b6c959f
Humairaa1 <humaira71bd@gmail.com>
2023-09-14 16:07:44 +0600

    First Commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-3xl font-bold'>Knowledge Cafe React</h1>
+        </div>
+    );
+};
+
+export default Header;
